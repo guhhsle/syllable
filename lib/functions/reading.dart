c6ae67e5b0341899434f0023f516ce3a6ca8f92c
guhhsle <guhhsle@proton.me>
2023-11-21 22:22:18 +0100

    epub support

--- lib/functions/reading.dart
+++ lib/functions/reading.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:archive/archive_io.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:syllable/functions.dart';
 
+import 'package:html/parser.dart';
 import '../data.dart';
 
 String text = '';
@@ -138,13 +141,47 @@ Future clearThreshold(bool auto) async {
 }
 
 Future<int> addBook() async {
+  String book = '';
+  late final PlatformFile result;
+
   try {
-    final result = await FilePicker.platform.pickFiles();
-    final book = utf8.decode(result!.files.single.bytes!);
-    setPref('book', book);
+    result = (await FilePicker.platform.pickFiles())!.files.single;
+//EPUB
+    if (result.name.endsWith('.epub')) {
+      try {
+        final inputStream = InputFileStream(result.path!);
+        final archive = ZipDecoder().decodeBuffer(inputStream);
+        for (var file in archive.files) {
+          if (file.isFile) {
+            if (file.name.endsWith('.html')) {
+              final String parsedString = parse(
+                parse(file.content).body!.text,
+              ).documentElement!.text;
+              book += '$parsedString\n\n\n';
+            }
+          }
+        }
+      } catch (e) {
+        debugPrint('$e');
+      }
+    } else {
+//TEXT
+      try {
+//APP
+        book = utf8.decode(await File(result.path!).readAsBytes());
+      } catch (e) {
+//WEB
+        try {
+          book = utf8.decode(result.bytes!);
+        } catch (e) {
+          showSnack('$e', false);
+        }
+      }
+    }
   } catch (e) {
     showSnack('$e', false);
   }
+  setPref('book', book);
   jumpTo(0);
   return 0;
 }
